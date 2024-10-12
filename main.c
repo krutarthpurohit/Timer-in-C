4bf11ee233b11afa38878cff848945ec1a64a80a
Krutarth P. <74556917+krutarthpurohit@users.noreply.github.com>
2024-10-12 11:16:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,45 +2,31 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
-#include <unistd.h> // For sleep function
+#include <unistd.h> 
 
 #define TIMER_SIG1 SIGUSR1
-#define TIMER_SIG2 SIGUSR2
 
 // Global variables to track timer ticks
 int timer1_ticks = 0;
-int timer2_ticks = 0;
 
 // Timer callback function
 void timer_callback(int sig) 
 {
+    printf("Time handler getting execuited\n");
     if (sig == TIMER_SIG1) 
     {
         printf("Timer 1 Ticks: %d\n", ++timer1_ticks);
     } 
-    else if (sig == TIMER_SIG2) 
-    {
-        printf("Timer 2 Ticks: %d\n", ++timer2_ticks);
-    }
 }
 
-int main() {
+void set_timer1(int delay_in_sec, int interval_in_sec)
+{
     // Set up signal handler for timer 1
     struct sigaction sa1;
     sa1.sa_flags = SA_SIGINFO;
     sa1.sa_sigaction = timer_callback;
     sigemptyset(&sa1.sa_mask);
     if (sigaction(TIMER_SIG1, &sa1, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up signal handler for timer 2
-    struct sigaction sa2;
-    sa2.sa_flags = SA_SIGINFO;
-    sa2.sa_sigaction = timer_callback;
-    sigemptyset(&sa2.sa_mask);
-    if (sigaction(TIMER_SIG2, &sa2, NULL) == -1) {
         perror("sigaction");
         exit(EXIT_FAILURE);
     }
@@ -56,49 +42,26 @@ int main() {
         perror("timer_create");
         exit(EXIT_FAILURE);
     }
-    
-    // Create timer 2
-    timer_t timerid2;
-    struct sigevent sev2;
-    struct itimerspec its2;
-    sev2.sigev_notify = SIGEV_SIGNAL;
-    sev2.sigev_signo = TIMER_SIG2;
-    sev2.sigev_value.sival_ptr = &timerid2;
-    if (timer_create(CLOCK_REALTIME, &sev2, &timerid2) == -1) 
-    {
-        perror("timer_create");
-        exit(EXIT_FAILURE);
-    }
-    
+       
     // Set timer 1 expiration time and interval
-    its1.it_value.tv_sec = 1; 
+    its1.it_value.tv_sec = delay_in_sec; 
     its1.it_value.tv_nsec = 0;
-    its1.it_interval.tv_sec = 4;
+    its1.it_interval.tv_sec = interval_in_sec;
     its1.it_interval.tv_nsec = 0;
-
-    // Set timer 2 expiration time and interval
-    its2.it_value.tv_sec = 1;
-    its2.it_value.tv_nsec = 0;
-    its2.it_interval.tv_sec = 2; 
-    its2.it_interval.tv_nsec = 0;
 
     // Start timer 1
     if (timer_settime(timerid1, 0, &its1, NULL) == -1) {
         perror("timer_settime");
         exit(EXIT_FAILURE);
     }
-
     printf("Timer 1 started. Waiting for ticks...\n");
-    // Start timer 2
-    if (timer_settime(timerid2, 0, &its2, NULL) == -1) {
-        perror("timer_settime");
-        exit(EXIT_FAILURE);
-    }
 
-    printf("Timer 2 started. Waiting for ticks...\n");
+}
+
+int main() {
     
-
-
+    set_timer1(10,0);       //delay for 10 seconds
+    
     // Print elapsed time every second
     int elapsed_seconds = 0;
     while (1) {
